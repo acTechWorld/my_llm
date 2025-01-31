16b90c2873b7ac151ab1d0da7c3188fc8994bd00
oncekiller <canard.antoine@gmail.com>
2025-02-01 00:22:13 +0100

    go back lama_cpp

--- api/llm.py
+++ api/llm.py
@@ -1,19 +1,9 @@
-from dotenv import load_dotenv
-import os
+from llama_cpp import Llama
 
-# Load environment variables
-load_dotenv()
+# Initialize the LLaMA model
+llama = Llama(model_path="/Users/antoinecanard/Projects/Python/my_llm/models/test/Meta-Llama-3.1-8B-Instruct-Q4_K_S.gguf")
 
-# Get the GGUF model path from .env
-from ctransformers import AutoModelForCausalLM
-
-# Set gpu_layers to the number of layers to offload to GPU. Set to 0 if no GPU acceleration is available on your system.
-model_id = "TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF"
-filename = "tinyllama-1.1b-chat-v1.0.Q6_K.gguf"
-
-llm = AutoModelForCausalLM.from_pretrained(model_id, model_file=filename)
-
-
-# Generate a response
 def generate_response(prompt):
-    return llm(prompt)
+    response = llama.generate(prompt)
+    print(response)
+    return response
