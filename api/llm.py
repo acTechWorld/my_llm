9e95530668ffdd4103f49fb201c5af8277e95d84
oncekiller <canard.antoine@gmail.com>
2025-01-31 23:18:40 +0100

    test AutoModel

--- api/llm.py
+++ api/llm.py
@@ -1,21 +1,19 @@
 from dotenv import load_dotenv
 import os
-from transformers import AutoTokenizer, AutoModelForCausalLM
+
 # Load environment variables
 load_dotenv()
 
 # Get the GGUF model path from .env
-model_path = os.getenv("MODEL_PATH")  # Update your .env file with MODEL_PATH
+from ctransformers import AutoModelForCausalLM
 
+# Set gpu_layers to the number of layers to offload to GPU. Set to 0 if no GPU acceleration is available on your system.
 model_id = "TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF"
 filename = "tinyllama-1.1b-chat-v1.0.Q6_K.gguf"
 
-tokenizer = AutoTokenizer.from_pretrained(model_id, gguf_file=filename)
-model = AutoModelForCausalLM.from_pretrained(model_id, gguf_file=filename)
+llm = AutoModelForCausalLM.from_pretrained(model_id, model_file=filename)
 
-# Create a Hugging Face pipeline
-pipe = pipeline("text-generation", model=model, tokenizer=tokenizer)
-llm = HuggingFacePipeline(pipeline=pipe)
 
+# Generate a response
 def generate_response(prompt):
-    return llm(prompt, max_new_tokens=150)
+    return llm(prompt)
