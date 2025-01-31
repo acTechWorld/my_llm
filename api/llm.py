5afead2060e741b0039b5ae36b294f38055200a3
oncekiller <canard.antoine@gmail.com>
2025-01-31 22:51:04 +0100

    test transformers

--- api/llm.py
+++ api/llm.py
@@ -1,14 +1,21 @@
 from dotenv import load_dotenv
 import os
-from gpt4all import GPT4All
+from transformers import AutoTokenizer, AutoModelForCausalLM
 # Load environment variables
 load_dotenv()
 
 # Get the GGUF model path from .env
 model_path = os.getenv("MODEL_PATH")  # Update your .env file with MODEL_PATH
 
-llm = GPT4All(model_path)
+model_id = "TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF"
+filename = "tinyllama-1.1b-chat-v1.0.Q6_K.gguf"
 
-# Function to generate responses
+tokenizer = AutoTokenizer.from_pretrained(model_id, gguf_file=filename)
+model = AutoModelForCausalLM.from_pretrained(model_id, gguf_file=filename)
+
+# Create a Hugging Face pipeline
+pipe = pipeline("text-generation", model=model, tokenizer=tokenizer)
+llm = HuggingFacePipeline(pipeline=pipe)
+
 def generate_response(prompt):
-    return llm.generate(prompt, max_tokens=150)
+    return llm(prompt, max_new_tokens=150)
